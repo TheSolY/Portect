1ff4e4cc2be6b077ad45eb32b96c9b72fa5de979
roieBS <roie.ben-simon@vayyar.com>
2024-05-11 18:01:55 +0300

    working computation of SER-FIQ

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -6,12 +6,17 @@ import torch.nn.functional as F
 import argparse
 from torchvision.io import read_image, ImageReadMode
 from torchvision.transforms import Resize
+from evaluations.backbones.iresnet import iresnet18
+from pathlib import Path
+from torchvision import io
+from torchvision import transforms
 
 from utils import FeatureExtractor
 
 IMAGE_SIZE = (640, 640)
 feature_extractor = FeatureExtractor()
 resize = Resize(IMAGE_SIZE)
+resize_112 = Resize((112,112))
 
 def compute_face_embedding(image_path):
     img = read_image(image_path, mode=ImageReadMode.RGB)
@@ -66,8 +71,34 @@ def calculate_FDFR_and_ISM(generated_images_path, original_images_path):
     return avg_ism, fail_detection_ratio
 
 
-avg_ism, fail_detection_ratio = calculate_FDFR_and_ISM('/content/Portect/assets/swapped_images', '/content/Portect/assets/org_images')
+def calculate_SER_FIQ_score(generated_images_path):
+    torch_device = torch.device("cpu")
+    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    image_list = os.listdir(generated_images_path)
+    SER_FIQ_scores = 0
+    for image_name in image_list:
+      image_path = os.path.join(generated_images_path, image_name)
+      resnet = iresnet18(dropout=0.4,num_features=512, use_se=False).to(torch_device)
+      resnet.load_state_dict(torch.load("evaluations/checkpoints/resnet18.pth", map_location=torch_device))
+      resnet.eval()
+      image = io.read_image(image_path)
+      image = resize_112(image)
+      image = image.type(torch.FloatTensor) / 255.0
+      image = normalize(image)
+      image = image.unsqueeze(0)
+      score = resnet.calculate_serfiq(image, T=10, scaling=5.0)
+      SER_FIQ_scores += score.item()
+    SER_FIQ_average_score = SER_FIQ_scores/len(image_list)
+    return SER_FIQ_average_score
+
+path_to_generated_images = '/content/Portect/assets/swapped_images'
+path_to_original_images = '/content/Portect/assets/org_images'
+
+avg_ism, fail_detection_ratio = calculate_FDFR_and_ISM(path_to_generated_images, path_to_original_images)
 print(avg_ism, fail_detection_ratio)
+SER_FIQ_average_score = calculate_SER_FIQ_score(path_to_generated_images)
+print(f"SER-FIQ Score: {SER_FIQ_average_score:.8f}")
+
 
 
 
