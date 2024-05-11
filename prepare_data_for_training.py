c34972eea0db43a7206be801a357c8eac8dcc8b2
TheSolY <solyarkoni@gmail.com>
2024-05-11 13:29:15 +0300

    Resize images to 512x512 during prepare dataset_for_training.py

--- prepare_data_for_training.py
+++ prepare_data_for_training.py
@@ -1,38 +1,44 @@
 import os
-from utils import FaceSwapper2, farthest_neighbor, interpolate_embedding
+import cv2
+from utils import FaceSwapper2, farthest_neighbor, interpolate_embedding, resize_img
 from diffusers.utils import load_image
 import numpy as np
 import torch
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# the data should be in a folder called "org_images" inside the root directory
+# the data should be in a folder called "src_images" inside the root directory
 images_root = './assets/'
 celeba_path = '/datasets/celeba'
 
-org_images_dir = os.path.join(images_root, 'org_images')
-if not os.path.exists(org_images_dir):
-    raise FileNotFoundError(f'{org_images_dir} not found')
+src_images_dir = os.path.join(images_root, 'src_images')
+if not os.path.exists(src_images_dir):
+    raise FileNotFoundError(f'{src_images_dir} not found')
 
-swapped_imaged_dir = os.path.join(images_root, 'swapped_images')
+org_image_dir = os.path.join(images_root, 'org_images')
+swapped_image_dir = os.path.join(images_root, 'swapped_images')
 
-if os.path.exists(swapped_imaged_dir):
-    raise FileExistsError(f'{swapped_imaged_dir} already exists')
+if os.path.exists(swapped_image_dir) or os.path.exists(org_image_dir):
+    raise FileExistsError(f'{swapped_image_dir} or {org_image_dir} already exists')
 else:
-    os.mkdir(swapped_imaged_dir)
-
-# Rename to indices for easy matching for the training later
-for i, image in enumerate(os.listdir(org_images_dir)):
-    _, file_extention = os.path.splitext(image)
-    if file_extention in ('.jpg', '.jpeg', '.png'):
-        os.rename(os.path.join(org_images_dir, image), os.path.join(org_images_dir, str(i) + file_extention))
+    os.mkdir(swapped_image_dir)
+    os.mkdir(org_image_dir)
 
 face_swapper = FaceSwapper2()
 
+# Resize the images and save by index for order matching during training,
 # Calculate the embedding centroid of the original images and find the target identity
 org_images_emb = []
-for filename in os.listdir(org_images_dir):
-    image = load_image(os.path.join(org_images_dir, filename))
-    emb = face_swapper.extract_features(image)
-    org_images_emb.append(emb)
+for i, filename in enumerate(os.listdir(src_images_dir)):
+    _, file_extention = os.path.splitext(filename)
+    if file_extention in ('.jpg', '.jpeg', '.png', '.JPG'):
+        image = load_image(os.path.join(src_images_dir, filename))
+        image = resize_img(image, 512, 512, pad_to_max_side=True)
+        img_to_save = np.array(image.copy())[:, :, ::-1]
+        cv2.imwrite(os.path.join(org_image_dir, str(i) + file_extention), img_to_save)
+
+        emb = face_swapper.extract_features(image)
+        org_images_emb.append(emb)
 
 org_images_emb = np.stack(org_images_emb)
 org_images_emb_centroid = org_images_emb.mean(axis=0)
@@ -42,18 +48,14 @@ id_identity_idxs = torch.load('assets/unique_ids')
 idx, vec = farthest_neighbor(org_images_emb_centroid, id_centroids.numpy())
 
 target_emb = interpolate_embedding(org_images_emb_centroid, vec, 0.05)
-# identity_idx = id_identity_idxs[idx].item()
 
 # Create the target (face swapped) images
-# Later replace the image for image with the embedding centroid, when we can face swap from embedding
-# target_id_filenames = all_image_path_from_index(identity_idx,  os.path.join(celeba_path, 'identity_CelebA.txt'))
-
-for image_filename in os.listdir(org_images_dir):
+for image_filename in os.listdir(org_image_dir):
     _, file_extention = os.path.splitext(image_filename)
-    if file_extention in ('.jpg', '.jpeg', '.png'):
-        face_swapper.swap_face(os.path.join(org_images_dir, image_filename),
+    if file_extention in ('.jpg', '.jpeg', '.png', '.JPG'):
+        face_swapper.swap_face(os.path.join(org_image_dir, image_filename),
                                target_emb,
-                               os.path.join(swapped_imaged_dir, image_filename))
+                               os.path.join(swapped_image_dir, image_filename))
 
 
 
